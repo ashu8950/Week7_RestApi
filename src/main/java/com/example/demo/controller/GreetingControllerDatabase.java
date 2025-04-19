8029f14d35316b55721e1ab0bf71992ed9ad16a2
Ashu <shabsingh961@gmail.com>
2025-04-20 01:58:18 +0530

    [ASHU]Add. UC7-> Edit existing data and update it

--- src/main/java/com/example/demo/controller/GreetingControllerDatabase.java
+++ src/main/java/com/example/demo/controller/GreetingControllerDatabase.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,6 +65,19 @@ public class GreetingControllerDatabase {
 	@GetMapping("/all")
 	public List<GreetingDto> getAllGreetings() {
 	    return greetingService.getAllGreetings();
+	}
+	
+	/*UC7->Ability for the Greeting
+
+	App to Edit a Greeting
+	
+	Messages in the
+	
+	Repository*/
+	
+	@PutMapping("/update")
+	public GreetingDto updateGreeting(@RequestBody GreetingDto request) {
+	    return greetingService.updateGreeting(request.getId(), request.getMessage());
 	}
 
 }
--- src/main/java/com/example/demo/service/GreetingService.java
+++ src/main/java/com/example/demo/service/GreetingService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
@@ -36,6 +37,7 @@ public class GreetingService implements IGreetingService {
         return greetingRepository.save(new GreetingDto(counter.incrementAndGet(), message));
     }
 	 
+	//fetching by id
 	
 	public GreetingDto getGreetingById(Long id) {
 		// TODO Auto-generated method stub
@@ -43,10 +45,24 @@ public class GreetingService implements IGreetingService {
 				.orElseThrow(()-> new RuntimeException("Greeting not found with ID: " + id));
 		return new GreetingDto(greeting.getId(),greeting.getMessage());
 	}
-	
+	//listing all data
 	 public List<GreetingDto> getAllGreetings() {
 	        return greetingRepository.findAll().stream()
 	                .map(greeting -> new GreetingDto(greeting.getId(), greeting.getMessage()))
 	                .collect(Collectors.toList());
 	    }
+	 
+	 //updating greeting
+	 public GreetingDto updateGreeting(Long id, String newMessage) {
+		    Optional<GreetingDto> optionalGreeting = greetingRepository.findById(id);
+
+		    if (optionalGreeting.isPresent()) {
+		        GreetingDto greeting = optionalGreeting.get();
+		        greeting.setMessage(newMessage);
+		        GreetingDto updatedGreeting = greetingRepository.save(greeting);
+		        return new GreetingDto(updatedGreeting.getId(), updatedGreeting.getMessage());
+		    } else {
+		        throw new RuntimeException("Greeting with ID " + id + " not found.");
+		    }
+		}
 }
